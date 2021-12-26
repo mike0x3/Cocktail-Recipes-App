4de7b2b3ac4a568480223ad9c4cebd294652789d
Mykhaylo Chornenkiy <mykhaylo.chornenkiy@twt-dh.de>
2021-12-26 16:06:20 +0100

    Initial Commit

---
+++ Cocktail Zoo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktail Zoo
+//
+//  Created by Mikhaylo Chornenkiy on 26.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
